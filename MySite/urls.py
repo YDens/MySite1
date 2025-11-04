2faf5f70b166cd864e7d26aeb20ce3247ae52554
YDens <ydenis2266@gmail.com>
2025-11-04 23:25:24 +0300

    add photo carcass and carousel

--- MySite/urls.py
+++ MySite/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from MySite import settings
 
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin1'),
     path('', include('main.urls', namespace='main')),
     path('user/', include('users.urls', namespace='user')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
